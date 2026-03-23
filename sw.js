c81884a230be0c4a197f1ce42011f19d4a8aec2a
kengooe-glitch <kengo.oe@gmail.com>
2026-03-23 17:17:20 +0900

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+// 耳鳴りトラッカー Service Worker
+const CACHE = 'tinnitus-tracker-v1';
+const ASSETS = [
+  './tinnitus-tracker.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.plot.ly/plotly-2.27.0.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
